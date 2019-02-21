b16217021d3af9354531acc17cd2776f66e2f333
Jessica Gladstone <jcg321@comcast.net>
2019-02-21 19:27:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-
    @user = User.new(username: params[:username], password: params[:password])
+   
    if @user.save && @user.username != ""
      redirect '/login'
-     
    else
      redirect to '/failure'
    end
